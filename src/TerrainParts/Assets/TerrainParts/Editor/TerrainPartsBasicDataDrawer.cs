a736f0102c76e6f51281318f78c10ca95407f782
DAIKI <daiki.evilone@gmail.com>
2024-02-02 13:01:03 +0900

    Replace basic data inspector draw script to PropertyDrawer.

---
+++ src/TerrainParts/Assets/TerrainParts/Editor/TerrainPartsBasicDataDrawer.cs
@@ -0,0 +1,73 @@
+using UnityEditor;
+using UnityEditor.UIElements;
+using UnityEngine.UIElements;
+
+namespace TerrainParts.Editor
+{
+    [CustomPropertyDrawer(typeof(TerrainPartsBasicData))]
+    public class TerrainPartsBasicDataDrawer : PropertyDrawer
+    {
+        private struct ToolCategoryChangedArgs
+        {
+            public readonly ToolCategory TargetToolCategory;
+            public readonly SerializedProperty SerializedProperty;
+
+            public ToolCategoryChangedArgs(ToolCategory targetToolCategory, SerializedProperty serializedProperty)
+            {
+                TargetToolCategory = targetToolCategory;
+                SerializedProperty = serializedProperty;
+            }
+        }
+
+        public override VisualElement CreatePropertyGUI(SerializedProperty property)
+        {
+            const float indentSize = 15f;
+
+            var container = new VisualElement();
+
+            var toolCategoryFlags = (ToolCategory)property.FindPropertyRelative("ToolCategory").enumValueFlag;
+            var enableHeightToggle = new Toggle("Edit Height");
+            enableHeightToggle.value = toolCategoryFlags.HasFlagAll(ToolCategory.Height);
+            enableHeightToggle.RegisterCallback<ChangeEvent<bool>, ToolCategoryChangedArgs>(OnToolCategoryChanged, new ToolCategoryChangedArgs(ToolCategory.Height, property));
+            container.Add(enableHeightToggle);
+
+            var enableTextureToggle = new Toggle("Edit Texture");
+            enableTextureToggle.value = toolCategoryFlags.HasFlagAll(ToolCategory.Texture);
+            enableTextureToggle.RegisterCallback<ChangeEvent<bool>, ToolCategoryChangedArgs>(OnToolCategoryChanged, new ToolCategoryChangedArgs(ToolCategory.Texture, property));
+            container.Add(enableTextureToggle);
+            var texturePropertyElement = new VisualElement();
+            texturePropertyElement.style.paddingLeft = indentSize;
+            texturePropertyElement.Add(new PropertyField(property.FindPropertyRelative("TextureLayerIndex"), "LayerIndex"));
+            texturePropertyElement.Add(new PropertyField(property.FindPropertyRelative("TextureLayerStrength"), "Strength"));
+            enableTextureToggle.RegisterCallback<ChangeEvent<bool>>(v => texturePropertyElement.style.display = v.newValue ? DisplayStyle.Flex : DisplayStyle.None);
+            container.Add(texturePropertyElement);
+
+
+            var enableHoleToggle = new Toggle("Edit Hole");
+            enableHoleToggle.value = toolCategoryFlags.HasFlagAll(ToolCategory.Hole);
+            enableHoleToggle.RegisterCallback<ChangeEvent<bool>, ToolCategoryChangedArgs>(OnToolCategoryChanged, new ToolCategoryChangedArgs(ToolCategory.Hole, property));
+            container.Add(enableHoleToggle);
+
+            container.Add(new PropertyField(property.FindPropertyRelative("WriteCondition")));
+            container.Add(new PropertyField(property.FindPropertyRelative("Layer")));
+            container.Add(new PropertyField(property.FindPropertyRelative("OrderInLayer")));
+            return container;
+        }
+
+        private void OnToolCategoryChanged(ChangeEvent<bool> boolEvent, ToolCategoryChangedArgs args)
+        {
+            args.SerializedProperty.serializedObject.Update();
+            var toolCategoryFlags = (ToolCategory)args.SerializedProperty.FindPropertyRelative("ToolCategory").enumValueFlag;
+            if (boolEvent.newValue)
+            {
+                toolCategoryFlags |= args.TargetToolCategory;
+            }
+            else
+            {
+                toolCategoryFlags &= ~args.TargetToolCategory;
+            }
+            args.SerializedProperty.FindPropertyRelative("ToolCategory").enumValueFlag = (int)toolCategoryFlags;
+            args.SerializedProperty.serializedObject.ApplyModifiedProperties();
+        }
+    }
+}
